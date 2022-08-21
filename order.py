f12fd19a492975e69412300277762e37cec8ab6c
jkingportfolio <mycodejourney3@gmail.com>
2022-08-21 10:51:24 +0000

    Add: delivery_time function based on order type

--- order.py
+++ order.py
@@ -39,8 +39,7 @@ class Order:
         print('\n'.join(map(str, self.order_list)))
         print()
         self.total_order_cost()
-        print(
-            f'\nYour order will be ready at {datetime.now() + timedelta(minutes=15)}\n')
+        self.delivery_time()
 
     def append_sales(self):
         """
@@ -64,10 +63,10 @@ class Order:
             print(f'The subtotal of your order is £{float(order_cost)}')
             order_cost = order_cost + delivery_charge
             print(f'There is a delivery charge of £{float(delivery_charge)}')
-            print(f'Total cost calculated with delivery £{float(order_cost)}')
+            print(f'Total cost calculated with delivery £{float(order_cost)}\n')
         else:
             print(f'The total cost of your order is £{order_cost}')
-            print('There is no delivery charge')
+            print('There is no delivery charge\n')
 
     def clear_screen(self):
         """
@@ -80,8 +79,16 @@ class Order:
         else:
             os.system('cls')
 
-    def order_times(self):
+    def delivery_time(self):
         """
         Calculate current time and delivery time
         """
-        
+        current_time = datetime.now()
+        order_ready_time = current_time + timedelta(minutes=15)
+        order_ready_time = order_ready_time.strftime("%Y-%m-%d %H:%M:%S")
+
+        if self.delivery_type == "DELIVERY":
+            print(f'Your order will be delivered at {order_ready_time}\n')
+        elif self.delivery_type == "COLLECTION":
+            print(
+                f'Your order will be ready for collection at {order_ready_time}\n')
