47d0880404ceada29b0688878b70280d42ed80b5
jkingportfolio <mycodejourney3@gmail.com>
2022-08-23 19:43:46 +0000

    Refact: Move GS consts to google_sheet.py

--- google_sheet.py
+++ google_sheet.py
@@ -1,3 +1,23 @@
 """
 Google sheets module
 """
+from tabulate import tabulate
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('taco_trailer')
+
+MENU = SHEET.worksheet("Menu").get_all_values()
+FORMATTED_MENU = (tabulate(MENU, headers=['Item', 'Name', 'Cost (£)'],
+                           tablefmt="simple", numalign="center"))
+SALES_WORKSHEET = SHEET.worksheet("Sales")
+PASSWORD = SHEET.worksheet("Password").acell('A1').value
--- run.py
+++ run.py
@@ -3,29 +3,12 @@ Main python file
 """
 import os
 import getpass
-import gspread
-from google.oauth2.service_account import Credentials
+from google_sheet import *
 import pyfiglet
-from tabulate import tabulate
+# from tabulate import tabulate
 from time import sleep
 from order import Order
 
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-]
-
-CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('taco_trailer')
-
-MENU = SHEET.worksheet("Menu").get_all_values()
-FORMATTED_MENU = (tabulate(MENU, headers=['Item', 'Name', 'Cost (£)'],
-                           tablefmt="simple", numalign="center"))
-SALES_WORKSHEET = SHEET.worksheet("Sales")
-PASSWORD = SHEET.worksheet("Password").acell('A1').value
 
 order_list = []
 name = None
