b7e04fad65de614debfd0ae6587c5c9af77c90a1
jkingportfolio <mycodejourney3@gmail.com>
2022-08-25 18:48:00 +0000

    Update: breaks in admin_dashboard

--- admin.py
+++ admin.py
@@ -7,11 +7,13 @@ def admin_dashboard():
         admin_option = input('Please select an option: ')
         if admin_option == '1':
             print('Option 1 selected')
+            break
         elif admin_option == '2':
             print('Option 2 selected')
+            break
         else:
             print('Thats not an option')
-        break
+        
 
 # def sales_info():
 
