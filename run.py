41c6fdcab96c30ac8899ba28ae014edde0f5ba8a
jkingportfolio <mycodejourney3@gmail.com>
2022-09-03 15:27:30 +0000

    Refact: Option style from () to []

--- run.py
+++ run.py
@@ -30,8 +30,8 @@ def welcome(message='Hello, would you like to place an order?\n'):
     print(message)
     while True:
         place_order = input(
-            'Please enter a valid input\n(Y) - Yes\n(N) - No\n'
-            '(A) - Admin Access\n').strip()
+            'Please enter a valid input\n[Y] - Yes\n[N] - No\n'
+            '[A] - Admin Access\n').strip()
         place_order = place_order.capitalize()
 
         if place_order == 'Y':
