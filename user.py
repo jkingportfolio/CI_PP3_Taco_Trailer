e18e687d11cc26372115dd2ca88a028515460db4
jkingportfolio <mycodejourney3@gmail.com>
2022-09-03 13:13:58 +0000

    Add: User class parameters required

--- user.py
+++ user.py
@@ -1,4 +1,14 @@
 class User:
     """
-    Class to create a new user in google sheets
+    Class that will create an order
     """
+
+    def __init__(self, admin_access, user_name, password,
+                    first_name, surname, address_number, address_street):
+        self.admin_access = admin_access
+        self.user_name = user_name
+        self.password = password
+        self.first_name = first_name
+        self.surname = surname
+        self.address_number = address_number
+        self.address_street = address_street
