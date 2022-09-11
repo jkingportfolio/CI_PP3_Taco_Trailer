ac7f7eae70201c3e49a12855cf95b069814bc07a
jkingportfolio <mycodejourney3@gmail.com>
2022-09-11 22:28:48 +0000

    Fix: Members area options display error

--- run.py
+++ run.py
@@ -401,8 +401,8 @@ def members_area(member_name, member_number, user_name):
     """
     end_func = True
     member_area_instructions = (colored('What would you'
-                                        ' like to do?', 'green'
-    '\n\n[1] - Make an order\n[2] - Change password\n[Q] - Log out\n'))
+                                        ' like to do?', 'green')) + \
+        '\n\n[1] - Make an order\n[2] - Change password\n[Q] - Log out\n'
     print(pyfiglet.figlet_format(f'Hi {member_name}'))
     print(member_area_instructions)
     while end_func:
