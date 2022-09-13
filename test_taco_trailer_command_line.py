bc3252d321705e315b0fc292de4ce169a7b6cb3f
jkingportfolio <mycodejourney3@gmail.com>
2022-09-13 11:21:31 +0000

    Add: Unit testing for password validation

---
+++ test_taco_trailer_command_line.py
@@ -0,0 +1,24 @@
+import unittest
+import taco_trailer_command_line as ttcl
+
+
+class TestPassword(unittest.TestCase):
+    """
+    Verification of the user password creation input
+    """
+
+    def test_validate_password(self):
+        """
+        Test for valid password input
+        """
+        self.assertTrue(ttcl.validate_password('Password1#'), True)
+
+    def test_validate_wrong_password(self):
+        """
+        Test for invalid password input
+        """
+        self.assertEqual(ttcl.validate_password('pass'), None)
+
+
+if __name__ == '__main__':
+    unittest.main()
